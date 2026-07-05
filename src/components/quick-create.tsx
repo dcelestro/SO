1f7147dddfa77fad822537268633c69f8025a254
dcelestro <dcelestro@gmail.com>
2026-07-05 16:19:03 -0300

    Wire quick create modal

--- src/components/quick-create.tsx
+++ src/components/quick-create.tsx
@@ -1,18 +1,3 @@
 "use client";
 
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
-
-export function QuickCreate({ open, onOpenChange }: { open: boolean; onOpenChange: (v: boolean) => void }) {
-  return (
-    <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="max-w-xl">
-        <DialogHeader>
-          <DialogTitle>Captura rápida</DialogTitle>
-          <DialogDescription>
-            La captura rápida queda reservada para tareas, proyectos, ideas y activos desde sus pantallas específicas.
-          </DialogDescription>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
-  );
-}
+export { QuickCreateModal as QuickCreate } from "@/components/quick-create/quick-create-modal";
